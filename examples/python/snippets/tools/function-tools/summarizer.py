0a6698754787077f83c838a4dfeed1def4cbf93a
adars124 <107804643+adars124@users.noreply.github.com>
2025-04-16 08:47:27 +0545

    docs: fix agent tool example doc (#93)
    Add  instead of  in the Runner

--- examples/python/snippets/tools/function-tools/summarizer.py
+++ examples/python/snippets/tools/function-tools/summarizer.py
@@ -25,7 +25,7 @@ root_agent = Agent(
 # Session and Runner
 session_service = InMemorySessionService()
 session = session_service.create_session(app_name=APP_NAME, user_id=USER_ID, session_id=SESSION_ID)
-runner = Runner(agent=summary_agent, app_name=APP_NAME, session_service=session_service)
+runner = Runner(agent=root_agent, app_name=APP_NAME, session_service=session_service)
 
 
 # Agent Interaction
